469217f811a5aa47a14ed9c941b75d11bc910192
josafat <josa221001@gmail.com>
2023-05-12 20:35:35 -0600

    add: archivos configuracion proyecto

---
+++ src/test/java/com/researchesapi/ResearchersApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.researchesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchersApiApplicationTests {
+    @Test
+    void contextLoads() {}
+}
